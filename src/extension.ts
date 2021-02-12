51c824b113d1cea53fb43fc18ac6575316bdcf8f
rpdswtk <szvacsi@gmail.com>
2021-02-12 23:22:25 +0100

    Removed devRefresh command

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,9 @@
 import * as vscode from 'vscode';
-import { MqttConnectionView } from './MqttConnectionView';
 import { VSMqttApp } from './VSMqttApp';
 
 
 export function activate(context: vscode.ExtensionContext) {
 	new VSMqttApp(context);
-
-	vscode.commands.registerCommand("vsmqtt.devRefresh", () => {
-		MqttConnectionView.kill();
-		setTimeout(() => {
-			MqttConnectionView.createOrShow(context.extensionUri, {
-				address: "localhost",
-				port: 1883,
-				name: "dev test"
-			});
-		}, 1000);
-	});
 }
 
 export function deactivate() { }
