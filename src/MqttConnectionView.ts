7bdd03f41cd0fbcefc72dc258dddb438a020d9a9
rpdswtk <szvacsi@gmail.com>
2021-02-15 21:54:25 +0100

    Added webview persistence

--- src/MqttConnectionView.ts
+++ src/MqttConnectionView.ts
@@ -38,6 +38,7 @@ export class MqttConnectionView {
             {
                 // Enable javascript in the webview
                 enableScripts: true,
+                retainContextWhenHidden: true,
 
                 // And restrict the webview to only loading content from our extension's `media` directory.
                 localResourceRoots: [
