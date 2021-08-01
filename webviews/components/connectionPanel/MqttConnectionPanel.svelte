bff5cdb2aef00159dd21d8e0fdef7f4588f6332a
rpdswtk <szvacsi@gmail.com>
2021-08-01 16:29:26 +0200

    Refactored connection panel

--- webviews/components/connectionPanel/MqttConnectionPanel.svelte
+++ webviews/components/connectionPanel/MqttConnectionPanel.svelte
@@ -7,9 +7,9 @@
     import SubscriptionList from "./SubscriptionList.svelte";
     import MessageOverview from "./MessageOverview.svelte";
     import { messages, selectedMessage, subscriptions, savedSubscriptions } from ".././stores";
+    import View from '../View.svelte';
 
     let brokerConfig: MqttBrokerConfig;
-    let connected: boolean = false;
 
     onMount(() => {
         brokerConfig = brokerProfile;
@@ -18,12 +18,6 @@
         window.addEventListener("message", (event) => {
             const message = event.data;
             switch (message.type) {
-                case "onMqttConnectionChange":
-                    connected = message.value.connected;
-                    break;
-                case "onMqttProfileChange":
-                    brokerConfig = message.value.brokerConfig;
-                    break;
                 case "onMqttMessage":
                     let subscription = $subscriptions.get(message.value.topic);
                     if (subscription && !subscription.muted) {
@@ -44,84 +38,65 @@
     });
 </script>
 
-<div id="content">
-    <div id="header">
-        {#if brokerConfig}
-            <h1 class="profile-name">MQTT profile: {brokerConfig.name}</h1>
-            {#if connected}
-                <h1 class="state">Connected</h1>
-            {:else}
-                <h1 class="state">Disconnected</h1>
-            {/if}
-            <br />
-        {/if}
-    </div>
-
-    <div id="publish-section" class="container">
-        <PublishSection />
-    </div>
-
-    <div id="subscribe-section" class="container">
-        <SubscribeSection />
-    </div>
-
-    {#if brokerConfig}
-        <div id="subscription-list-section" class="container">
-            <SubscriptionList profileName={brokerConfig.name} />
+<View>
+    <div id="content">
+        <div id="publish-section" class="container">
+            <PublishSection />
         </div>
-    {/if}
-
-    <div id="message-section" class="container">
-        <MessageList />
+    
+        <div id="subscribe-section" class="container">
+            <SubscribeSection />
+        </div>
+    
+        {#if brokerConfig}
+            <div id="subscription-list-section" class="container">
+                <SubscriptionList profileName={brokerConfig.name} />
+            </div>
+        {/if}
+    
+        <div id="message-section" class="container">
+            <MessageList />
+        </div>
+    
+        <div id="message-overview-section" class="container">
+            <MessageOverview />
+        </div>
     </div>
-
-    <div id="message-overview-section" class="container">
-        <MessageOverview />
-    </div>
-</div>
+</View>
 
 <style>
     #content {
         height: 100%;
         display: grid;
         grid-template-columns: 1fr 2fr;
-        grid-template-rows: min-content min-content min-content 40vh 20vh;
-    }
-
-    #header {
-        grid-area: 1 / 1 / 2 / 3;
-        margin-left: 5px;
-        margin-right: 5px;
-    }
-
-    .profile-name {
-        float: left;
-    }
-
-    .state {
-        float: right;
+        grid-template-rows: min-content min-content 40vh 20vh;
+        grid-template-areas:
+            "publish publish"
+            "subscribe subscribe"
+            "subscription messages"
+            "subscription message-overview";
     }
 
     #publish-section {
-        grid-area: 2 / 1 / 3 / 3;
+        grid-area: publish;
     }
 
     #subscribe-section {
-        grid-area: 3 / 1 / 4 / 3;
+        grid-area: subscribe;
     }
 
     #subscription-list-section {
-        grid-area: 4 / 1 / 6 / 2;
+        grid-area: subscription;
         overflow: scroll;
     }
 
     #message-section {
-        grid-area: 4 / 2 / 5 / 3;
+        grid-area: messages;
         height: 100%;
     }
 
     #message-overview-section {
-        grid-area: 5 / 2 / 6 / 3;
+        grid-area: message-overview;
         border-top: None;
     }
 
