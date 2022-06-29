b4f1e7c5defe95b53c8f74db2220923041a69bb5
rpdswtk <szvacsi@gmail.com>
2022-06-29 23:55:56 +0200

    Test setup

---
+++ src/tests/commands.test.ts
@@ -0,0 +1,15 @@
+import assert = require("assert");
+import { Workbench, InputBox } from "vscode-extension-tester";
+
+describe('Dummy tests', () => {
+
+    it('Passing test', async () => {
+        await new Workbench().executeCommand('add broker profile');
+        assert.equal(1, 1);
+    });
+
+    it('Failing test', async () => {
+        await new Workbench().executeCommand('add broker profile');
+        assert.equal(1, 0);
+    });
+});
