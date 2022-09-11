cbd754a8f552f5bb92db03ee9f642a3632801dcb
rpdswtk <szvacsi@gmail.com>
2022-09-11 12:43:40 +0200

    Fixed remove broker profile command

---
+++ src/interfaces/MqttProfileQuickPickItem.ts
@@ -0,0 +1,4 @@
+import { QuickPickItem } from "vscode";
+import { MqttBrokerConfig } from "./MqttBrokerConfig";
+
+export interface MqttProfileQuickPickItem extends QuickPickItem, MqttBrokerConfig {}
