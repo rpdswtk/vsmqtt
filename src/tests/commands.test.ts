a4ef0d938b81efac31706f6dfdfbb38562b94ef2
rpdswtk <szvacsi@gmail.com>
2022-10-07 23:39:59 +0200

    Restructured tests

--- src/tests/commands.test.ts
+++ src/tests/commands.test.ts
@@ -38,100 +38,108 @@ describe('Commands', function () {
         await sleep(500);
     });
 
-    it('"Add broker profile" saves profile to settings.json', async function () {
-        await new Workbench().executeCommand('add broker profile');
-
-        let input = await InputBox.create();
-        await input.wait();
-        await input.setText(BROKER_PROFILE.name);
-        await input.confirm();
-        await input.setText(BROKER_PROFILE.host);
-        await input.confirm();
-        await input.setText(BROKER_PROFILE.port.toString());
-        await input.confirm();
-
-        await sleep(2000);
-        const rawData = fs.readFileSync(path.join(projectPath, '.vscode/settings.json'));
-        const settings = JSON.parse(rawData.toString());
-
-        const savedProfile = settings['vsmqtt.brokerProfiles'][0];
-
-        expect(savedProfile.name).to.equal(BROKER_PROFILE.name);
-        expect(savedProfile.host).to.equal(BROKER_PROFILE.host);
-        expect(savedProfile.port).to.equal(BROKER_PROFILE.port);
+    describe('Add broker profile', () => {
+        it('saves profile to settings.json', async function () {
+            await new Workbench().executeCommand('add broker profile');
+    
+            let input = await InputBox.create();
+            await input.wait();
+            await input.setText(BROKER_PROFILE.name);
+            await input.confirm();
+            await input.setText(BROKER_PROFILE.host);
+            await input.confirm();
+            await input.setText(BROKER_PROFILE.port.toString());
+            await input.confirm();
+    
+            await sleep(2000);
+            const rawData = fs.readFileSync(path.join(projectPath, '.vscode/settings.json'));
+            const settings = JSON.parse(rawData.toString());
+    
+            const savedProfile = settings['vsmqtt.brokerProfiles'][0];
+    
+            expect(savedProfile.name).to.equal(BROKER_PROFILE.name);
+            expect(savedProfile.host).to.equal(BROKER_PROFILE.host);
+            expect(savedProfile.port).to.equal(BROKER_PROFILE.port);
+        });
     });
 
-    it('"Edit broker profile" opens settings.json', async function () {
-        await new Workbench().executeCommand('edit broker profile');
-        await sleep(2000);
-
-        const editorView = new EditorView();
-
-        const titles = await editorView.getOpenEditorTitles();
-
-        expect(titles).to.contain('settings.json');
+    describe('Edit broker profile', () => {
+        it('opens settings.json', async function () {
+            await new Workbench().executeCommand('edit broker profile');
+            await sleep(2000);
+    
+            const editorView = new EditorView();
+    
+            const titles = await editorView.getOpenEditorTitles();
+    
+            expect(titles).to.contain('settings.json');
+        });
     });
 
-    it('"Remove broker profile" removed profile from settings.json', async function() {
-        createSettingsWithProfile();
-
-        await new Workbench().executeCommand("remove broker profile");
-        const input = await InputBox.create();
-        await sleep(2000);
-
-        await input.selectQuickPick(0);
-
-        await sleep(2000);
-
-        const dialog = new ModalDialog();
-        await VSBrowser.instance.waitForWorkbench();
-        await dialog.pushButton("Yes");
-
-        await VSBrowser.instance.openResources(path.join(projectPath, '.vscode/settings.json'));
-        const settingsFile = await new EditorView().openEditor('settings.json');
-
-        const settingsText = await settingsFile.getText();
-
-        const settings = JSON.parse(settingsText);
-
-        expect(settings["vsmqtt.brokerProfiles"]).to.not.deep.include.members([BROKER_PROFILE]);
+    describe('Remove broker profile', () => {
+        it('removes profile from settings.json', async function() {
+            createSettingsWithProfile();
+    
+            await new Workbench().executeCommand("remove broker profile");
+            const input = await InputBox.create();
+            await sleep(2000);
+    
+            await input.selectQuickPick(0);
+    
+            await sleep(2000);
+    
+            const dialog = new ModalDialog();
+            await VSBrowser.instance.waitForWorkbench();
+            await dialog.pushButton("Yes");
+    
+            await VSBrowser.instance.openResources(path.join(projectPath, '.vscode/settings.json'));
+            const settingsFile = await new EditorView().openEditor('settings.json');
+    
+            const settingsText = await settingsFile.getText();
+    
+            const settings = JSON.parse(settingsText);
+    
+            expect(settings["vsmqtt.brokerProfiles"]).to.not.deep.include.members([BROKER_PROFILE]);
+        });
     });
 
-    it('"Connect to mqtt broker" connects to broker', async function () {
-        createSettingsWithProfile();
-        startBroker();
+    describe("Connect to mqtt broker", () => {
+        it('connects to broker', async function () {
+            createSettingsWithProfile();
+            startBroker();
+    
+            await new Workbench().executeCommand("Connect to mqtt broker");
+            const input = await InputBox.create();
+            await input.selectQuickPick(0);
+    
+            const webview = await new EditorView().openEditor('VSMQTT');
+            webview.wait();
+            const mqttView = new WebView();
+            await mqttView.switchToFrame();
+    
+            const connectionState = await mqttView.findWebElement(By.className('state'));
+            expect(await connectionState.getText()).to.equal('Connected');
+            await mqttView.switchBack();
+    
+            stopBroker();
+        });
 
-        await new Workbench().executeCommand("Connect to mqtt broker");
-        const input = await InputBox.create();
-        await input.selectQuickPick(0);
-
-        const webview = await new EditorView().openEditor('VSMQTT');
-        webview.wait();
-        const mqttView = new WebView();
-        await mqttView.switchToFrame();
-
-        const connectionState = await mqttView.findWebElement(By.className('state'));
-        expect(await connectionState.getText()).to.equal('Connected');
-        await mqttView.switchBack();
-
-        stopBroker();
-    });
-
-    it('"Connect to mqtt broker" prompts for password', async function () {
-        createSettingsWithProfile({ promptCredentials: true });
-        await new Workbench().executeCommand("Connect to mqtt broker");
-        const input = await InputBox.create();
-        await input.selectQuickPick(0);
-
-        await sleep(2000);
-
-        expect(await (await input.getMessage()).startsWith('Username'), 'Should be username').to.be.true;
-        await input.setText('user');
-        await input.confirm();
-
-        await input.wait();
-        expect(await (await input.getMessage()).startsWith('Password'), 'Should be password').to.be.true;
-        await input.cancel();
+        it('prompts for password', async function () {
+            createSettingsWithProfile({ promptCredentials: true });
+            await new Workbench().executeCommand("Connect to mqtt broker");
+            const input = await InputBox.create();
+            await input.selectQuickPick(0);
+    
+            await sleep(2000);
+    
+            expect(await (await input.getMessage()).startsWith('Username'), 'Should be username').to.be.true;
+            await input.setText('user');
+            await input.confirm();
+    
+            await input.wait();
+            expect(await (await input.getMessage()).startsWith('Password'), 'Should be password').to.be.true;
+            await input.cancel();
+        });
     });
 
     const createSettingsWithProfile = (propertyOverrides = {}) => {
