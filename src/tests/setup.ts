6277efa8e989315b6b70d77b058417e2b4ea4ff0
rpdswtk <szvacsi@gmail.com>
2023-09-17 22:01:39 +0200

    Fix rimraf

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -2,19 +2,13 @@ import path = require("path")
 import { TEST_PROJECT_FOLDER } from "./utils/constants"
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const rimraf = require("rimraf")
+const { rimraf } = require("rimraf")
 
 const removeTestFolders = async () => {
   console.log("Removing project folders")
-  await rimraf(
-    path.join(__dirname, TEST_PROJECT_FOLDER + "*"),
-    function (error: Error) {
-      if (error) {
-        console.log("Could not remove test project folder")
-        console.log(error)
-      }
-    }
-  )
+  await rimraf(path.join(__dirname, TEST_PROJECT_FOLDER + "*"), {
+    glob: true,
+  })
 }
 
 after(async function () {
