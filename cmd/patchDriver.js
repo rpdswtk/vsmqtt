6e8bc7fcaad2ee23389e9f48fd7c436338a2c8a5
rpdswtk <31532753+rpdswtk@users.noreply.github.com>
2025-12-14 14:27:21 +0100

    Update pipeline

---
+++ cmd/patchDriver.js
@@ -0,0 +1,23 @@
+const chrome = require("selenium-webdriver/chrome")
+const fs = require("fs")
+const path = require("path")
+
+const OriginalServiceBuilder = chrome.ServiceBuilder
+
+if (process.env.TEST_VERBOSE != "True") {
+  return
+}
+
+chrome.ServiceBuilder = function (driverPath) {
+  const instance = new OriginalServiceBuilder(driverPath)
+
+  console.log("---------------------------------------------------------")
+  console.log("SUCCESS: Captured ServiceBuilder via NODE_OPTIONS!")
+  console.log("---------------------------------------------------------")
+
+  instance.enableVerboseLogging()
+  instance.loggingTo("logs/chromedriver.log")
+  return instance
+}
+
+chrome.ServiceBuilder.prototype = OriginalServiceBuilder.prototype
